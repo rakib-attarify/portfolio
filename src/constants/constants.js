e6c3277912fd5ccfba802661c6cb44e5d1b9a758
Ayeman <ayemanbsalauddin@gmail.com>
2022-10-19 21:38:37 +0600

    image

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Attarify",
     description:
       "Planned and Managed products as company's requirements grew. Started as Cheif Fields Executive and later got promoted to Chief Operating Officer within a short period of time.",
-    image: "/images/1.png",
+    image: "/images/1.jpg",
     tags: ["Product Development", "Product Management", "Production Planning"],
     visit: "https://www.attarify.com",
     id: 0,
